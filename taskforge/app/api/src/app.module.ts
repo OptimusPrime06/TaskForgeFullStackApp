5e110f99f0f5c5df9023334a69bfd475e93b95fe
OptimusPrime <gulliverraed@gmail.com>
2026-04-04 01:36:05 +0200

    App Configuration
    - Updated .env file with access token, refresh token secrets and expiry time.
    - Updated .env file with DB important data; host name, port, username, password, and name.
    - Installed 4 packages; @nestjs/typeorm typeorm ( for converting entities code to DB tables ), pg ( to communicate with PostgreSQL ), and @nestjs/jwt to manage access and refresh tokens.
    - Updated app.module.ts with the ConfigModule and TypeOrmModule.

--- taskforge/app/api/src/app.module.ts
+++ taskforge/app/api/src/app.module.ts
@@ -5,9 +5,33 @@ import { AuthModule } from './auth/auth.module';
 import { ProjectsModule } from './projects/projects.module';
 import { TasksModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule, ProjectsModule, TasksModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,        
+      envFilePath: '../../../.env', 
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get('DB_USERNAME'),
+        password: config.get('DB_PASSWORD'),
+        database: config.get('DB_NAME'),
+        autoLoadEntities: true,  
+        synchronize: true,       
+      }),
+    }),
+    AuthModule,
+    ProjectsModule,
+    TasksModule, 
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

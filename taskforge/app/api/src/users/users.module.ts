0ea029b5f183904adfc0cfa43e92bab485776b00
OptimusPrime <gulliverraed@gmail.com>
2026-04-06 21:44:07 +0200

    Added UserController to User module

--- taskforge/app/api/src/users/users.module.ts
+++ taskforge/app/api/src/users/users.module.ts
@@ -4,11 +4,13 @@ import { User } from './data/entities/user.entity';
 import { UserRepository } from './data/repositories/user.repository';
 import { UsersService } from './business.logic/services/users.service';
 import { IUserRepository } from './business.logic/interfaces/i.user.repository';
+import { UsersController } from './presentation/controllers/users.controller';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([User]),
     ],
+    controllers: [UsersController],
     providers: [
         {
             provide: IUserRepository,
